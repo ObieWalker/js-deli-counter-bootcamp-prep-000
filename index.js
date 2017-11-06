20076e0861efa84944497c2ccc1fcdbbc3a1d789
Obi <obinnawalker@gmail.com>
2017-11-06 10:06:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(lineArray){
-  if (lineArray === 0){
+  if (lineArray.length === 0){
     return "There is nobody waiting to be served!"
   }
   var next = lineArray[0]
   lineArray.shift()
-  return "Currently serving "+next
+  return "Currently serving "+next "."
 }
