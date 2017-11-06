4826a90f112a770225b9fdf2ba63a2577d07add4
Obi <obinnawalker@gmail.com>
2017-11-06 10:42:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,13 +3,23 @@ function takeANumber(katzDeliLine, name){
   return "Welcome, "+name+". You are number " + katzDeliLine.length +" in line."
 }
 
-function nowServing(lineArray){
-  if (lineArray.length === 0){
+function nowServing(line){
+  if (line.length === 0){
     return "There is nobody waiting to be served!"
+  }else{
+    var lineObject = {};
+    for (var i=0; i<line.length; i++){
+      lineObject[i+1] = line[i]
+    }
+    console.log(lineObject)
   }
-  var next = lineArray[0]
-  lineArray.shift()
-  return "Currently serving "+next +"."
+  var item = ''
+  var arr = ["The line is currently: "]
+  for (var key in lineObject){
+    item = key + ": " + lineObject[key]
+    arr.push(item)
+  }
+  return arr;
 }
 
 function currentLine(katzDeliLine){
